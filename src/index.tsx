4ddc11ac50548f0189f2ff4fcd3c508e289621b3
Sakir Ali <sakirali2011@gmail.com>
2024-09-16 06:36:28 +0530

    Lint removed

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import OrientationComponent from './components/OrientationComponent';
 import reportWebVitals from './reportWebVitals';
 
